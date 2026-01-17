08e615fa2a09822edd6a5b7561f51b226bf6d17f
Aboubacar Konate <akonateboucar198@gmail.com>
2026-01-17 09:49:46 +0000

    Modification de la page Vendeur

--- resources/views/PageVendeur.blade.php
+++ resources/views/PageVendeur.blade.php
@@ -7,15 +7,30 @@
 </head>
 <body>
     @csrf
-    <header>
-        <img src="Logo-Site.png" width="200" alt="Logo de la plateforme" title="LOGO" class="logo"><br/>
-    </header>
-    <br/>
-     <nav class="nav">
+    <!--<header>
+        <img src="Logo-Site.png" width="200" alt="Logo de la plateforme" title="LOGO" class="logo">
+    </header>-->
+     <!--<nav class="nav">
+        <ul>
+            <li><a href="{{''}}">Produits</a></li>
+            <li><a href="{{''}}">Commandes</a></li>
+        </ul>
+    </nav>-->
+    <button id="menuBtn">☰</button>
+    <nav id="sidebar">
         <ul>
             <li><a href="{{''}}">Produits</a></li>
             <li><a href="{{''}}">Commandes</a></li>
         </ul>
     </nav>
+    <script>
+        const menuBtn = document.getElementById("menuBtn");
+        const sidebar = document.getElementById("sidebar");
+
+        menuBtn.addEventListener("click", () => 
+        {
+            sidebar.classList.toggle("active")
+        });
+    </script>
 </body>
 </html>
